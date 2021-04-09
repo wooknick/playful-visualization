1260631294f90b656e3f3249383fbb7f70b849dd
wooknick <wooknick@naver.com>
2021-04-09 20:14:04 +0900

    update layout

--- src/2/app.js
+++ src/2/app.js
@@ -1,58 +1,117 @@
 import TextRoll from "./textRoll";
+import MainImage from "./images/main.png";
+import SubImage from "./images/sub.png";
 
 function getDummy() {
   const data = [
-    "Get the Top",
-    "Not Bad",
-    "Garbage",
-    "Wasting Time",
-    "Amazing!!",
-    "The Return of the King",
+    // "Get the Top",
+    // "Not Bad",
+    // "Garbage",
+    // "Wasting Time",
+    // "Amazing!!",
+    // "The Return of the King",
+    "Expected position is No.50",
+    "Expected position is No.1",
+    "Expected position is No.350",
   ];
 
   return data[Math.floor(Math.random() * data.length)];
 }
 
-function init() {
-  // Variables
-  const v = { mainArtist: "", subArtist: "" };
+const artists = ["dua lipa", "eminem", "bts", "muse", "radiohead", "beatles"];
 
-  // HTML Elements
-  const mainInput = document.getElementById("mainInput");
-  const subInput = document.getElementById("subInput");
-  const beforeResult = document.getElementById("beforeResult");
-  const result = document.getElementById("result");
+class App {
+  constructor() {
+    // HTML Elements
+    this.inputLine = document.getElementById("inputLine");
+    this.contentLine = document.getElementById("contentLine");
+    this.mainInput = document.getElementById("mainInput");
+    this.subInput = document.getElementById("subInput");
+    this.mainImage = document.getElementById("mainImage");
+    this.subImage = document.getElementById("subImage");
+    this.beforeResult = document.getElementById("beforeResult");
+    this.result = document.getElementById("result");
+    // Variables
+    this.artists = { mainArtist: "", subArtist: "" };
+    this.animating = false;
+    this.inputT = undefined;
+    this.resizeT = undefined;
+    this.size = window.innerWidth / 32;
+  }
 
-  // TextRoll
-  const textRoll = new TextRoll(result, window.innerWidth / 32);
-  textRoll.setText("Try your Self");
+  init() {
+    // TextRoll
+    this.textRoll = new TextRoll(result, this.size);
+    this.textRoll.setText("Try your Self");
 
-  // Event handler
-  let t;
+    this.resize();
+    // Event handler
+    window.addEventListener("resize", this.debouncedResize.bind(this));
+    this.mainInput.addEventListener("input", this.handleInput.bind(this));
+    this.subInput.addEventListener("input", this.handleInput.bind(this));
+  }
 
-  function handleInput(e) {
+  handleInput(e) {
     const { value, name } = e.target;
-    v[name] = value;
-    beforeResult.innerHTML = `${v.mainArtist} + ${v.subArtist}`;
+    this.artists[name] = value;
+    this.beforeResult.innerHTML = `<span>${this.artists.mainArtist}</span><span>feat. ${this.artists.subArtist}</span>`;
 
-    if (v.mainArtist !== "" && v.subArtist !== "") {
-      if (t) {
-        clearTimeout(t);
+    if (name === "mainArtist") {
+      this.mainImage.style.opacity = 0;
+      this.mainImage.ontransitionend = () => {
+        this.mainImage.innerHTML = `<image src="${MainImage}"></image>`;
+        this.mainImage.style.opacity = 1;
+      };
+    } else {
+      this.subImage.style.opacity = 0;
+      this.subImage.ontransitionend = () => {
+        this.subImage.innerHTML = `<image src="${SubImage}"></image>`;
+        this.subImage.style.opacity = 1;
+      };
+    }
+
+    if (this.artists.mainArtist !== "" && this.artists.subArtist !== "") {
+      if (this.inputT) {
+        clearTimeout(this.inputT);
       }
-      t = setTimeout(() => {
-        mainInput.disabled = true;
-        subInput.disabled = true;
-        textRoll.animateTo(getDummy(), 1000);
+      this.inputT = setTimeout(() => {
+        this.animating = true;
+        this.mainInput.disabled = true;
+        this.subInput.disabled = true;
+        this.textRoll.animateTo(getDummy(), 1000);
         setTimeout(() => {
-          mainInput.disabled = false;
-          subInput.disabled = false;
+          this.animating = false;
+          this.mainInput.disabled = false;
+          this.subInput.disabled = false;
         }, 2500);
       }, 1200);
     }
   }
 
-  document.getElementById("mainInput").addEventListener("input", handleInput);
-  document.getElementById("subInput").addEventListener("input", handleInput);
+  resize() {
+    this.size = window.innerWidth / 32;
+    this.inputLine.style.fontSize = this.size + "px";
+    this.contentLine.style.fontSize = this.size + "px";
+    this.beforeResult.style.fontSize = this.size * 2 + "px";
+    this.textRoll.updateSize(this.size);
+  }
+
+  debouncedResize() {
+    if (this.resizeT) {
+      clearTimeout(this.resizeT);
+    }
+    if (this.animating) {
+      this.resizeT = setTimeout(() => {
+        this.resize();
+      }, 2500 + 500);
+    } else {
+      this.resizeT = setTimeout(() => {
+        this.resize();
+      }, 500);
+    }
+  }
 }
 
-window.onload = init;
+window.onload = () => {
+  new App().init();
+};
